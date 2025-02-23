3a0bbde224f7eecbd514164f53fcc41186873b59
Oleg Pliskin Trifonov <Bapvometh90@yandex.ru>
2025-02-23 22:57:05 +0300

    First commit

---
+++ ewm-service/src/main/java/ru/practicum/comment/service/AdminCommentService.java
@@ -0,0 +1,17 @@
+package ru.practicum.comment.service;
+
+import jakarta.validation.constraints.Positive;
+import org.springframework.data.domain.PageRequest;
+import ru.practicum.comment.dto.AdminCommentOutputDto;
+import ru.practicum.comment.dto.AdminStatusUpdateCommentInputDto;
+
+import java.util.List;
+
+public interface AdminCommentService {
+    void adminDeleteComment(Long commentId);
+
+    AdminCommentOutputDto adminUpdateCommentStatus(Long commentId,
+                                                   AdminStatusUpdateCommentInputDto adminStatusUpdateCommentInputDto);
+
+    List<AdminCommentOutputDto> getAuthorComments(Long authorId, PageRequest pageRequest);
+}
